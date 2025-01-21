d399682622728a41497273e5fe5bcab7a134147d
Gabriel Paulo Tavares da Silva <gabrielpt201111@hotmail.com>
2025-01-21 08:25:01 -0300

    feat: adicionando setup da tela relatorio

---
+++ src/app/relatorio/page.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { DataTable } from "@/components/DataTable";
+import { Layout } from "@/components/Layout";
+import { useCallback, useMemo } from "react";
+
+type Data = {
+  id: number;
+  name: string;
+};
+
+export default function RelatorioPage() {
+  const data: Data[] = useMemo(() => [
+    {
+      id: 1,
+      name: "Relatório de Funcionários"
+    },
+    {
+      id: 2,
+      name: "Relatório de Vale Refeição"
+    },
+    {
+      id: 3,
+      name: "Relatório de Vale Transporte"
+    }
+  ], []);
+
+  function handleNextPage() {
+    console.log("next page");
+  }
+
+  function handleBackPage() {
+    console.log("back page");
+  }
+
+  const handleEdit = useCallback((row: Data) => {
+    console.log("edit", row);
+  }, []);
+
+  const handleDelete = useCallback((row: Data) => {
+    console.log("delete", row);
+  }, []);
+
+  const handleView = useCallback((row: Data) => {
+    console.log("view", row);
+  }, []);
+
+
+  return (
+    <Layout pageTitle="Relatórios">
+      <DataTable
+        data={data}
+        perPage={10}
+        total={10}
+        page={1}
+        onNextPageClick={() => handleNextPage()}
+        onBackPageClick={() => handleBackPage()}
+        onEditClick={handleEdit}
+        onDeleteClick={handleDelete}
+        onViewClick={handleView}
+      />
+    </Layout>
+  );
+}
