589f853026410368b8a2a0a37c4872a0246cdcf1
Gabriel Paulo Tavares da Silva <gabrielpt201111@hotmail.com>
2025-01-21 09:11:48 -0300

    feat: adicionando env baseURL

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 
 export default function middleware(req: NextRequest) {
 
-  const dashboardURL = new URL("/dashboard", "http://localhost:3000");
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+  const dashboardURL = new URL("/dashboard", baseUrl);
 
   if (req.nextUrl.pathname === "/") {
     return NextResponse.redirect(dashboardURL);
